f760412065175590f69d4d69d33056beda3d1c29
geraldin <geraldin>
2017-05-14 12:23:24 -0500

    prueba1

---
+++ 2014139598-SNL/2014139598-CON.Console/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2014139598_CON.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ 2014139598-SNL/2014139598-PER-Persistence/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2014139598_PER_Persistence
+{
+    public class Class1
+    {
+    }
+}
